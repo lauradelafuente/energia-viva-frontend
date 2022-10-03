5bccfc236c23156f6e8c316927fe572cde7f4ad3
Luca Andrey <lucaandrey5@hotmail.com>
2022-10-03 17:05:52 -0300

    mudancas na navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,66 +1,88 @@
-import { AppBar, Toolbar, Box, Typography, Grid } from '@mui/material';
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'
+import { AppBar, Toolbar, Box, Typography, Grid } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import IconButton from "@mui/material/IconButton";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 function Navbar() {
   return (
     <>
       <AppBar position="static">
-        <Toolbar variant="dense" className='navbar'>
+        <Toolbar variant="dense" className="navbar">
           <Box className="cursor">
-            <img src="https://o.remove.bg/downloads/025981c1-1e89-4fcb-88af-731636f3f058/ENERGIA_VIVA-removebg-preview.png" alt="" className='logo'/>
+            <img
+              src="https://o.remove.bg/downloads/025981c1-1e89-4fcb-88af-731636f3f058/ENERGIA_VIVA-removebg-preview.png"
+              alt=""
+              className="logo"
+            />
           </Box>
 
-          <Grid container justifyContent="flex">
-            <Box display="flex" justifyContent="start" >
-            <Link to='/home' className='sublinhado'>
-              <Box mx={1} className="cursor fonte">
-                <Typography variant="h6" color="inherit" fontFamily='Bangers cursive'>
-                  Home
-                </Typography>
-              </Box>
+          <Grid container>
+            <Box display="flex">
+              <Link to="/home" className="sublinhado">
+                <Box mx={1} className="cursor fonte">
+                  <Typography
+                    variant="h6"
+                    color="inherit"
+                    fontFamily="Bangers cursive"
+                  >
+                    Home
+                  </Typography>
+                </Box>
               </Link>
 
               <Box mx={1} className="cursor">
-                <Typography variant="h6" color="inherit" fontFamily='Bangers cursive'>
+                <Typography
+                  variant="h6"
+                  color="inherit"
+                  fontFamily="Bangers cursive"
+                >
                   Categorias
                 </Typography>
               </Box>
 
               <Box mx={1} className="cursor">
-                <Typography variant="h6" color="inherit" fontFamily='Bangers cursive'>
+                <Typography
+                  variant="h6"
+                  color="inherit"
+                  fontFamily="Bangers cursive"
+                >
                   Promoções
                 </Typography>
               </Box>
 
-              <Box mx={1} className="cursor">
-                <Typography variant="h6" color="inherit" fontFamily='Bangers cursive'>
+              <Box mx={1} className="cursor" marginLeft={85}>
+                <Typography
+                  variant="h6"
+                  color="inherit"
+                  fontFamily="Bangers cursive"
+                >
                   Sobre nós
                 </Typography>
               </Box>
 
-              <Link to='/login' className='sublinhado'>
-              <Box mx={1} className="cursor">
-                <Typography variant="h6" color="inherit" fontFamily='Bangers cursive'>
-                  Login
-                </Typography>
-              </Box>
+              <Link to="/login" className="sublinhado">
+                <Box mx={1} className="cursor">
+                  <Typography
+                    variant="h6"
+                    color="inherit"
+                    fontFamily="Bangers cursive"
+                  >
+                    Login
+                  </Typography>
+                </Box>
               </Link>
 
-              <a href="https://www.instagram.com/generationbrasil/?hl=pt-br" className='sublinhado'>
               <Box mx={1} className="cursor">
-                <Typography variant="h6" color="inherit" fontFamily='Bangers cursive'>
-                  Instagram
-                </Typography>
+                <IconButton
+                  color="primary"
+                  aria-label="add to shopping cart"
+                  className="carButton"
+                >
+                  <AddShoppingCartIcon />
+                </IconButton>
               </Box>
-              </a>
-              
-                  <Box mx={1} className="cursor" >
-                      <Typography variant="h6" color="inherit" fontFamily='Bangers cursive'>
-                        Carrinho
-                      </Typography>
-                  </Box>
             </Box>
           </Grid>
         </Toolbar>
