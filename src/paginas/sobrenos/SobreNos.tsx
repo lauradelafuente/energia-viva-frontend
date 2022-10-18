aed3be2db553382a54c5380b5a7c267fde3633f2
Luca Andrey <lucaandrey5@hotmail.com>
2022-10-18 16:05:33 -0300

    corrigindo sobre nos

--- src/paginas/sobrenos/SobreNos.tsx
+++ src/paginas/sobrenos/SobreNos.tsx
@@ -1,178 +1,268 @@
-import React from 'react';
+import React from "react";
 import "./SobreNos.css";
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import { Box, Grid } from '@mui/material';
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import { Box, Grid } from "@mui/material";
 
 function SobreNos() {
   return (
-    <Grid container spacing={3} className="container">
-    <div className='container'>
-  <Grid item xs={12} sm={6} md={4} lg={2}>
-  <div className="card card0">
-    <div className="border">
-      <h2>Laura De La Fuente</h2>
-      <div className="icons">
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <GitHubIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <LinkedInIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      </div>
-    </div>
-  </div>
-  </Grid>
-  <Grid item xs={12} sm={6} md={4} lg={2}>
-  <div className="card card1">
-    <div className="border">
-      <h2>Luca Andrey</h2>
-      <div className="icons">
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <GitHubIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <LinkedInIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      </div>
-    </div>
-  </div>
-  </Grid>
-  <Grid item xs={12} sm={6} md={4} lg={2}>
-  <div className="card card2">
-    <div className="border">
-      <h2>Kelvyn Farias</h2>
-      <div className="icons">
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <GitHubIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <LinkedInIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      </div>
-    </div>
-  </div>
-  </Grid>
-  </div>
-  <Grid item xs={12} sm={6} md={4} lg={2}>
-  <div className="card card3">
-    <div className="border">
-      <h2>Larissa Lacerda</h2>
-      <div className="icons">
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <GitHubIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <LinkedInIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      </div>
-    </div>
-  </div>
-  </Grid>
-  <Grid item xs={12} sm={6} md={4} lg={2}>
-  <div className="card card4">
-    <div className="border">
-      <h2>Gustavo Henrique</h2>
-      <div className="icons">
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <GitHubIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <LinkedInIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      </div>
-    </div>
-  </div>
-  </Grid>
-  <Grid item xs={12} sm={6} md={4} lg={2}>
-  <div className="card card5">
-    <div className="border">
-      <h2>Henrique Leonardo</h2>
-      <div className="icons">
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <GitHubIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      <Box display="flex" alignItems="center" justifyContent="center">
-        <a
-        href="https://github.com/lucaandrey/energia-viva-frontend"
-        target="_blank"
-        rel="noreferrer"
-        >
-      <LinkedInIcon style={{ fontSize: 40, color: '#FDC921' }} aria-hidden="true"/>
-      </a>
-      </Box>
-      </div>
-    </div>
-    </div>
-  </Grid>
-</Grid>
-  )
+    <>
+      <Grid container display="flex" justifyContent="flex-start">
+        <div className="container">
+          <Grid item xs={12} sm={6} md={4} lg={2}>
+            <div className="card card0">
+              <div className="border">
+                <h2>Laura De La Fuente</h2>
+                <div className="icons">
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <GitHubIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <LinkedInIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                </div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={2}>
+            <div className="card card1">
+              <div className="border">
+                <h2>Luca Andrey</h2>
+                <div className="icons">
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <GitHubIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <LinkedInIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                </div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={2}>
+            <div className="card card2">
+              <div className="border">
+                <h2>Kelvyn Farias</h2>
+                <div className="icons">
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <GitHubIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <LinkedInIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                </div>
+              </div>
+            </div>
+          </Grid>
+        </div>
+      </Grid>
+      <Grid container>
+        <div className="container">
+          <Grid item xs={12} sm={6} md={4} lg={2}>
+            <div className="card card3">
+              <div className="border">
+                <h2>Larissa Lacerda</h2>
+                <div className="icons">
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <GitHubIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <LinkedInIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                </div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={2}>
+            <div className="card card4">
+              <div className="border">
+                <h2>Gustavo Henrique</h2>
+                <div className="icons">
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <GitHubIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <LinkedInIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                </div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4} lg={2}>
+            <div className="card card5">
+              <div className="border">
+                <h2>Henrique Leonardo</h2>
+                <div className="icons">
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <GitHubIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <a
+                      href="https://github.com/lucaandrey/energia-viva-frontend"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <LinkedInIcon
+                        style={{ fontSize: 40, color: "#FDC921" }}
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </Box>
+                </div>
+              </div>
+            </div>
+          </Grid>
+        </div>
+      </Grid>
+    </>
+  );
 }
 
-export default SobreNos
+export default SobreNos;
