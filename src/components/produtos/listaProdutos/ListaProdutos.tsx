6ca835e8ff8d90bbb9283ce3061b5df92fe652f2
Luca Andrey <lucaandrey5@hotmail.com>
2022-10-18 23:10:42 -0300

    estilizando card da listagem de produtos e searchbar esta funcionando!!

--- src/components/produtos/listaProdutos/ListaProdutos.tsx
+++ src/components/produtos/listaProdutos/ListaProdutos.tsx
@@ -9,63 +9,62 @@ import {
   Container,
   Grid,
 } from "@mui/material";
-import React, { useEffect, useState, ChangeEvent } from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import Produtos from "../../../models/Produtos";
 import { busca, post } from "../../../service/Service";
 import { TokenState } from "../../../store/tokens/tokensReducer";
 import Navbar from "../../estaticos/navbar/Navbar";
-import './ListaProdutos.css'
-import TextInfoContent from '@mui-treasury/components/content/textInfo';
-import { useBlogTextInfoContentStyles } from '@mui-treasury/styles/textInfoContent/blog';
-import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over';
-import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import "./ListaProdutos.css";
+import { makeStyles } from "@material-ui/core/styles";
+import cx from "clsx";
 
 const useStyles = makeStyles(({ breakpoints, spacing }) => ({
   root: {
-    margin: 'auto',
+    // margin: "auto",
     borderRadius: spacing(2), // 16px
-    transition: '0.3s',
-    boxShadow: '0px 14px 80px rgba(34, 35, 58, 0.2)',
-    position: 'relative',
+    transition: "0.3s",
+    boxShadow: "0px 14px 80px rgba(34, 35, 58, 0.2)",
+    position: "relative",
     maxWidth: 500,
-    marginLeft: 'auto',
-    overflow: 'initial',
-    background: '#ffffff',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    // marginLeft: "auto",
+    overflow: "initial",
+    background: "#6798C0",
+    display: "flex",
+    // flexDirection: "column",
+    justifyContent: "space-around",
+    alignItems: "center",
     paddingBottom: spacing(2),
-    [breakpoints.up('md')]: {
-      flexDirection: 'row',
+    [breakpoints.up("md")]: {
+      flexDirection: "row",
       paddingTop: spacing(2),
     },
   },
   media: {
-    width: '88%',
-    marginLeft: 'auto',
-    marginRight: 'auto',
+    width: "88%",
+    marginLeft: "auto",
+    marginRight: "auto",
     marginTop: spacing(-3),
     height: 0,
-    paddingBottom: '48%',
+    paddingBottom: "48%",
     borderRadius: spacing(2),
-    backgroundColor: '#fff',
-    position: 'relative', 
-    [breakpoints.up('md')]: {
-      width: '100%',
+    backgroundColor: "#fff",
+    position: "relative",
+    [breakpoints.up("md")]: {
+      width: "100%",
       marginLeft: spacing(-3),
       marginTop: 0,
-      transform: 'translateX(-8px)',
+      transform: "translateX(-8px)",
     },
-    '&:after': {
+    "&:after": {
       content: '" "',
-      position: 'absolute',
+      position: "absolute",
       top: 0,
       left: 0,
-      width: '100%',
-      height: '100%',
-      backgroundImage: 'linear-gradient(147deg, #fe8a39 0%, #fd3838 74%)',
+      width: "100%",
+      height: "100%",
+      // backgroundImage: "linear-gradient(147deg, #fe8a39 0%, #fd3838 74%)",
       borderRadius: spacing(2), // 16
       opacity: 0.5,
     },
@@ -75,22 +74,23 @@ const useStyles = makeStyles(({ breakpoints, spacing }) => ({
   },
   cta: {
     marginTop: 24,
-    textTransform: 'initial',
+    textTransform: "initial",
   },
 }));
 
 function ListaProdutos(props: any) {
   let navigate = useNavigate();
 
-  const [produtos, setProdutos] = useState<any[]>([]);
-
-  const [search, setSearch] = useState("");
+  const [produtos, setProdutos] = useState<Produtos[]>([]);
 
   const token = useSelector<TokenState, TokenState["tokens"]>(
     (state) => state.tokens
   );
-  let filter: string = props.inputText;
-  let vazio: boolean = false;
+  let filter = props.inputText;
+
+  console.log(filter);
+
+  console.log();
 
   useEffect(() => {
     if (token === "") {
@@ -106,55 +106,36 @@ function ListaProdutos(props: any) {
       },
     });
   }
-
   useEffect(() => {
     getProduto();
   }, [produtos.length]);
 
-  const productFilter = (name: ChangeEvent<HTMLInputElement>) => {
-    setSearch(name.target.value);
-  };
-
-  const filteredList = produtos.filter((elements) => {
-    if (filter === "") {
-      return elements;
-    } else {
-      console.log(elements.nomeProduto.toLowerCase().includes(filter));
-      if (elements !== "") {
-        return (vazio = true);
-      }
-      return elements.nomeProduto.toLowerCase().includes(filter);
-    }
-  });
-
   const styles = useStyles();
-  const {
-    button: buttonStyles,
-    ...contentStyles
-  } = useBlogTextInfoContentStyles();
-  const shadowStyles = useOverShadowStyles();
 
   return (
     <>
-    <Card className={cx(styles.root, shadowStyles.root)}>
-      <CardMedia
-        className={styles.media}
-        image={
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Git_icon.svg/2000px-Git_icon.svg.png'
-        }
-      />
-      <CardContent>
-        <TextInfoContent
-          classes={contentStyles}
-          overline={'28 MAR 2019'}
-          heading={'What is Git ?'}
-          body={
-            'Git is a distributed version control system. Every dev has a working copy of the code and...'
-          }
-        />
-        <Button className={buttonStyles}>Read more</Button>
-      </CardContent>
-    </Card>
+      {produtos
+        .filter((produtos) => {
+          return produtos.nomeProduto.toLowerCase().includes(filter);
+        })
+        .map((produtos) => (
+          <Grid container justifyContent="space-around" display="flex">
+            <Grid item xs={12} sm={6} m={4} lg={4}>
+              <Card className={cx(styles.root)}>
+                <CardMedia
+                  className={styles.media}
+                  image={`${produtos.potencia}`}
+                />
+                <CardContent>
+                  <Button className="ok">Atualizar</Button>
+                  <Typography color="textSecondary" gutterBottom>
+                    {produtos.nomeProduto}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        ))}
     </>
   );
 }
